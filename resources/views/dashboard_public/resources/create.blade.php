3fcf39e30f744ff7e8120bae8174d042afabb8c8
VanesaFn <vanefndzt@gmail.com>
2022-10-02 11:34:19 -0500

    Creacion vista de up resource

--- resources/views/dashboard_public/resources/create.blade.php
+++ resources/views/dashboard_public/resources/create.blade.php
@@ -7,21 +7,80 @@
   <title>Document</title>
   <!-- Core JS Files Bootstrap -->
   <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/stilosR.css') }}" rel="stylesheet">
   <!-- Core CSS Files Bootstrap-->
 	<script src="{{ asset('atlantis/assets/js/core/jquery.3.2.1.min.js') }}"></script>
 	<script src="{{ asset('atlantis/assets/js/core/popper.min.js') }}"></script>
 	<script src="{{ asset('atlantis/assets/js/core/bootstrap.min.js') }}"></script>
 </head>
 <body>
-  <div class="row">
-    <div class="col-6">
-      adasdsadas
+    <div class="container-fluid">
+    
+        <div class="row">
+            <div class="col-4">
+            </div>
+            <div class="col">
+                <nav class="navbar navbar-expand-sm">
+                </nav>
+            </div>
+            <div class="span">
+                <div class="col">
+                    <nav class="navbar navbar-expand-sm">
+                        <ul class="navbar-nav">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Bienvenid@</a>
+                            </li>
+                                <a class="nav-link" href="#">Usuario</a>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <div class="offset-4 text-center">
+            <nav class="navbar navbar-expand-sm">
+                <h1>Subir un recurso</h1>
+            </nav>
+        </div>
+        <form>
+            <div class="row mt-5">
+                <div class="col-4 offset-1 ">
+                    <label>Nombre del recurso</label>
+                    <input type="text" class="form-control" placeholder="Nombre" id="nombreid">
+                </div>
+                <div class="col-4 offset-1">
+                    <label>Seleccione categoria</label>
+                    <select class="form-control" id="selectCate">
+                        <option>Ciencia</option>
+                        <option>Analitica</option>
+                        <option>Predicción</option>
+                        <option>Programación</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col-4 offset-1">
+                    <label>Descripción</label>
+                    <textarea rows="4" type="text" class="form-control" placeholder="Descripción" id="descrip"></textarea>
+                </div>
+                <div class="col-4 offset-2">
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <button class="button-34" type="submit">Registrar</button>
+                </div>
+            </div>
+            <div class="row mt-4">
+                <div class="col-4 offset-1">
+                    <label>Contenido del recurso</label>
+                    <input type="file" class="form-control" id="ContenR"></input>
+                </div>
+                <div class="col-12 float-right  text-right pr-5" style="margin-top: -310px;">
+                    <img src="{{ asset('img/coheteR.png')}}" width="400" alt="">
+                </div>
+            </div>
+        </form>
+        
     </div>
-    <div class="col-6">
-      <div class="row">
-        sfsfsdsdf
-      </div>  
-    </div>
-  </div>
 </body>
 </html>
