4405f10a97461d8c51df0b8fe2960638bb61c18c
Hanna Obando <hannaobandopk@gmail.com>
2022-10-02 14:41:57 -0500

    adelanto

--- resources/views/public/team/index.blade.php
+++ resources/views/public/team/index.blade.php
@@ -7,21 +7,49 @@
   <title>Document</title>
   <!-- Core JS Files Bootstrap -->
   <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/app_public.css') }}" rel="stylesheet">
   <!-- Core CSS Files Bootstrap-->
 	<script src="{{ asset('atlantis/assets/js/core/jquery.3.2.1.min.js') }}"></script>
 	<script src="{{ asset('atlantis/assets/js/core/popper.min.js') }}"></script>
 	<script src="{{ asset('atlantis/assets/js/core/bootstrap.min.js') }}"></script>
 </head>
 <body>
+  <div class="container-fluid">
+    <div class="row" style="z-index: 40;">
+        <div class="col-3 offset-4  ">
+            <nav class="navbar navbar-expand-sm">
+                <ul class="navbar-nav ">
+                    <li class="nav-item ">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link" href="#">About us</a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link" href="#">Resources</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <div class="col-2 offset-2">
+            <div class="span">
+                <nav class="navbar navbar-expand-sm float-right">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#"><u>Log in</u></a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
   <div class="row">
-    <div class="col-6">
-      adasdsadas
+    <div class="col-12 text-center">
+      <h3>Team</h3>
     </div>
-    <div class="col-6">
-      <div class="row">
-        sfsfsdsdf
-      </div>  
-    </div>
+  </div>
+  <div class="row">
+    <img src="{{ asset('img/user_-removebg-preview.png') }}" alt="" width="450">
   </div>
 </body>
 </html>
